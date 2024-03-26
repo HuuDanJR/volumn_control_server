efb4e9f4aecc730e331916e4feb9f3173f63bb5f
huudan.jr <40542971+HuuDanJR@users.noreply.github.com>
2024-03-26 09:35:59 +0700

    push all code

--- app.js
+++ app.js
@@ -61,8 +61,15 @@ app.post('/create_volume', async (req, res) => {
 // Route to list all records
 app.get('/list_volume', async (req, res) => {
   try {
-    // const volumes = await volumeModel.find().sort({ id: 1 }).lean(); 
-    const volumes = await volumeModel.find({type:0}).sort({ id: 1 }).lean(); // Sorting by id in ascending order (ASC)
+    // const { type } = req.query;
+    // let query = { type: { $in: [0] } }; // Default query for type = 0
+    // if (type === '0' || type === '2') {
+    //   query = { type: parseInt(type) }; // Modify query if type is 1 or 2
+    // }
+
+    // const volumes = await volumeModel.find(query).sort({ id: 1 }).lean();
+    const volumes = await volumeModel.find({ type: { $in: [0, 2] } }).sort({ id: 1 }).lean();
+    // const volumes = await volumeModel.find({ type: 0 }).sort({ id: 1 }).lean(); // Sorting by id in ascending order (ASC)
     volumes.sort((a, b) => parseInt(a.id) - parseInt(b.id));
     if (!volumes || volumes.length === 0) {
       res.status(404).send({ "status": false, "message": `no volumes found `, "data": null });
@@ -156,9 +163,9 @@ app.post('/update_volume_value/:id', async (req, res) => {
     );
 
     if (!volume) {
-      res.status(404).send({"status": false, "message": "volume not found", "data": null });
+      res.status(404).send({ "status": false, "message": "volume not found", "data": null });
     } else {
-      res.status(200).send({"status": true, "message": "currentValue updated successfully", "data": volume });
+      res.status(200).send({ "status": true, "message": "currentValue updated successfully", "data": volume });
     }
   } catch (err) {
     res.status(500).send({ "status": false, "message": "fail to update currentValue", "data": null });
@@ -176,69 +183,167 @@ app.get('/list_preset', async (req, res) => {
     if (!presets || presets.length === 0) {
       res.status(404).send({ "status": false, "message": `no presets found `, "data": null });
     } else {
-      res.status(200).send({ "status": true, "message": `presets found ${presets.length}`,"createAt":createdAt, "data": presets });
+      res.status(200).send({ "status": true, "message": `presets found ${presets.length}`, "createAt": createdAt, "data": presets });
     }
   } catch (err) {
     res.status(500).send({ "status": false, "message": "fail to retrieve presets", "data": null });
   }
 });
 
+
+// Route to get a specific preset by its _id
+app.get('/preset_update_volume_value/:id', async (req, res) => {
+  const baseUrl = `${req.protocol}://${req.get('host')}`;
+  try {
+    const presetId = req.params.id;
+    const preset = await presetsModel.findById(presetId).populate('volumes').lean();
+    const listVolumeResponse = await axios.get(`${baseUrl}/list_volume?type=0`);
+    const volumesFromAPI = listVolumeResponse.data.data;
+    console.log(`Data from list_volume APIs:`, volumesFromAPI.length);
+    console.log(`Data from preset:`, preset.volumes.length);
+    const presetVolumes = preset.volumes;
+    const apiVolumes = volumesFromAPI;
+    const minLength = Math.min(presetVolumes.length, apiVolumes.length);
+    const updatePromises = [];
+    for (let i = 0; i < minLength; i++) {
+      const presetVolume = presetVolumes[i];
+      const apiVolume = apiVolumes[i];
+      // Check if the volumes match based on their IDs and names
+      if (presetVolume.id.toString() === apiVolume.id.toString()) {
+        console.log('APIs volumes id:', apiVolume._id);
+        console.log('preset volumes current value:', presetVolume.currentValue);
+        // Update currentValue of presetVolume with value from apiVolume
+        updatePromises.push(axios.post(`${baseUrl}/update_volume/${apiVolume._id}`, {
+          currentValue: presetVolume.currentValue
+        }));
+      }
+    }
+    // Execute all update requests in parallel
+    await Promise.all(updatePromises);
+
+    if (!preset) {
+      return res.status(404).send({ "status": false, "message": `Preset with _id ${presetId} not found`, "data": null });
+    }
+    return res.status(200).send({ "status": true, "message": `Update all  ${preset.presetName} preset's  volume value  as current successfull`, "data": preset });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ "status": false, "message": "Failed to retrieve the preset", "data": null });
+  }
+});
+
+
+
+
+
 // create a new preset
 app.post('/create_preset', async (req, res) => {
   try {
-    const {  volumes, presetId,presetName } = req.body;
+    const { volumes, presetId, presetName } = req.body;
     const existingRecord = await presetsModel.findOne({ presetId, presetName, });
     if (existingRecord) {
       return res.status(400).send({ "status": false, "message": "A preset with the same id, name  already exists", "data": null });
     }
     const presets = new presetsModel(req.body);
     await presets.save();
-    res.status(201).send({"status": true, "message": "Preset created successfully", "data": presets});
+    res.status(201).send({ "status": true, "message": "Preset created successfully", "data": presets });
   } catch (err) {
     res.status(500).send({ "status": false, "message": "Internal server error", "data": null });
   }
 });
 
-// create a new preset
+// // create a new preset
+// app.post('/create_preset_fix', async (req, res) => {
+//   // Get the base URL dynamically
+//   const baseUrl = `${req.protocol}://${req.get('host')}`;
+//   try {
+//     const { presetId, presetName } = req.body;
+//     const volumesResponse = await axios.get(`${baseUrl}/list_volume`); // Assuming you're using Axios
+//     const volumesData = volumesResponse.data.data;
+//     const copiedVolumes = [];
+//     for (const volumeData of volumesData) {
+//       // Create a copy of the volume with modified properties
+//       const copiedVolume = new volumeModel({
+//         ...volumeData,
+//         _id: mongoose.Types.ObjectId(), // Generate a new ObjectId for the copied volume
+//         name: `${volumeData.name} .`, // Modify the name
+//         presetId: '', // Initialize presetId as empty
+//         type: 1, // Set type to 1
+//       });
+
+//       // Save the copied volume
+//       await copiedVolume.save();
+
+//       // Push the copied volume to the array
+//       copiedVolumes.push(copiedVolume);
+//     }
+
+//     const newPreset = new presetsModel({
+//       presetId,
+//       presetName,
+//       volumes: copiedVolumes // Assign the copied volume models to the preset's volumes field
+//     });
+
+//     // Save the new preset
+//     await newPreset.save();
+
+//     // Update the presetId field for each copied volume
+//     for (const copiedVolume of copiedVolumes) {
+//       copiedVolume.presetId = newPreset._id; // Set the presetId to the new preset's _id
+//       await copiedVolume.save(); // Save the updated volume
+//     }
+
+//     res.status(201).send({
+//       "status": true,
+//       "message": "Preset created successfully",
+//       "data": newPreset
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send({
+//       "status": false,
+//       "message": "Internal server error",
+//       "data": null
+//     });
+//   }
+// });
+
 app.post('/create_preset_fix', async (req, res) => {
   // Get the base URL dynamically
   const baseUrl = `${req.protocol}://${req.get('host')}`;
   try {
     const { presetId, presetName } = req.body;
-    const volumesResponse = await axios.get(`${baseUrl}/list_volume`); // Assuming you're using Axios
+
+    // Get volumes data
+    const volumesResponse = await axios.get(`${baseUrl}/list_volume`);
     const volumesData = volumesResponse.data.data;
-    const copiedVolumes = [];
-    for (const volumeData of volumesData) {
-      // Create a copy of the volume with modified properties
-      const copiedVolume = new volumeModel({
-        ...volumeData,
-        _id: mongoose.Types.ObjectId(), // Generate a new ObjectId for the copied volume
-        name: `${presetName}(${volumeData.name})`, // Modify the name
-        presetId: '', // Initialize presetId as empty
-        type: 1, // Set type to 1
-      });
 
-      // Save the copied volume
-      await copiedVolume.save();
+    // Create copied volumes
+    const copiedVolumes = volumesData.map(volumeData => ({
+      ...volumeData,
+      _id: mongoose.Types.ObjectId(), // Generate a new ObjectId for the copied volume
+      name: `${volumeData.name} .`, // Modify the name
+      presetId: '', // Initialize presetId as empty
+      type: 1 // Set type to 1
+    }));
 
-      // Push the copied volume to the array
-      copiedVolumes.push(copiedVolume);
-    }
+    // Bulk insert copied volumes
+    await volumeModel.insertMany(copiedVolumes);
 
+    // Create new preset with copied volumes
     const newPreset = new presetsModel({
       presetId,
       presetName,
-      volumes: copiedVolumes // Assign the copied volume models to the preset's volumes field
+      volumes: copiedVolumes.map(volume => volume._id) // Assign the copied volume ids to the preset's volumes field
     });
-    
+
     // Save the new preset
     await newPreset.save();
 
-    // Update the presetId field for each copied volume
-    for (const copiedVolume of copiedVolumes) {
-      copiedVolume.presetId = newPreset._id; // Set the presetId to the new preset's _id
-      await copiedVolume.save(); // Save the updated volume
-    }
+    // Bulk update presetId for copied volumes
+    await volumeModel.updateMany(
+      { _id: { $in: copiedVolumes.map(volume => volume._id) } }, // Filter volumes by their ids
+      { presetId: newPreset._id } // Update presetId to the new preset's _id
+    );
 
     res.status(201).send({
       "status": true,
@@ -260,17 +365,17 @@ app.post('/create_preset_fix', async (req, res) => {
 router.delete('/delete_preset/:presetId', async (req, res) => {
   const { presetId } = req.params;
   try {
-      // Find the preset by ID and delete it
-      const deletedPreset = await presetsModel.findByIdAndDelete(presetId);
-      if (!deletedPreset) {
-          // If preset is not found, return 404 Not Found
-          return res.status(404).json({ success: false, message: 'Preset not found',data:null });
-      }
-      // If preset is successfully deleted, return success response
-      return res.status(200).json({ success: true, message: `Preset deleted successfully `,data:deletedPreset });
+    // Find the preset by ID and delete it
+    const deletedPreset = await presetsModel.findByIdAndDelete(presetId);
+    if (!deletedPreset) {
+      // If preset is not found, return 404 Not Found
+      return res.status(404).json({ success: false, message: 'Preset not found', data: null });
+    }
+    // If preset is successfully deleted, return success response
+    return res.status(200).json({ success: true, message: `Preset deleted successfully `, data: deletedPreset });
   } catch (error) {
-      // If an error occurs during deletion, return 500 Internal Server Error
-      return res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message });
+    // If an error occurs during deletion, return 500 Internal Server Error
+    return res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message });
   }
 });
 // Route to delete a volume by ID
@@ -293,26 +398,26 @@ router.put('/update_preset/:presetId', async (req, res) => {
   const { presetName, volumes } = req.body;
 
   try {
-      // Find the preset by ID
-      let preset = await presetsModel.findById(presetId);
+    // Find the preset by ID
+    let preset = await presetsModel.findById(presetId);
 
-      if (!preset) {
-          // If preset is not found, return 404 Not Found
-          return res.status(404).json({ success: false, message: 'Preset not found' });
-      }
+    if (!preset) {
+      // If preset is not found, return 404 Not Found
+      return res.status(404).json({ success: false, message: 'Preset not found' });
+    }
 
-      // Update preset properties
-      preset.presetName = presetName || preset.presetName;
-      preset.volumes = volumes || preset.volumes;
+    // Update preset properties
+    preset.presetName = presetName || preset.presetName;
+    preset.volumes = volumes || preset.volumes;
 
-      // Save the updated preset
-      preset = await preset.save();
+    // Save the updated preset
+    preset = await preset.save();
 
-      // Return success response with updated preset
-      return res.status(200).json({ success: true, message: 'Preset updated successfully', preset });
+    // Return success response with updated preset
+    return res.status(200).json({ success: true, message: 'Preset updated successfully', preset });
   } catch (error) {
-      // If an error occurs during update, return 500 Internal Server Error
-      return res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message });
+    // If an error occurs during update, return 500 Internal Server Error
+    return res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message });
   }
 });
 
